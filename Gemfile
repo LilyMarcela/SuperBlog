9770aa8ae2b0246c08ad8d060b852bf79cc2d2bc
Lili <marcela.b.abril@gmail.com>
2018-05-06 23:34:25 -0500

    adding annotate gem

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem 'annotate'
 
 gem 'friendly_id', '~> 5.1.0'
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id               :integer          not null, primary key
+#  title            :string
+#  body             :text
+#  description      :text
+#  slug             :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  banner_image_url :string
+#  author_id        :integer
+#
+
 class Post < ApplicationRecord
 
   extend FriendlyId
